c9112e450a37cdcf0afd4562f4fb5b668f1327d3
NBdiMAN <907645377@qq.com>
2020-09-26 13:11:50 +0800

    Initial commit

---
+++ src/StringTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author wangyang
+ * @date 2020/9/26 13:09
+ * @description:通过断点测试String的不可变性
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "张三";
+        str = "李四";
+    }
+}
